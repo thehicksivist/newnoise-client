c1557bd205e1999dc73a6ce0b53b3ab123e39ff4
thehicksivist <hicks.a.james@gmail.com>
2019-03-28 20:59:07 +1000

    input change still needs work

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,10 +5,16 @@ const api = 'http://localhost:3001/posts'
 
 class Posts extends React.Component {
 
-    state = {
-        posts: [],
-        newPost: ''
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      newPost: {title: '', image: '', text: ''}
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+    
     
       componentDidMount() {
         axios.get(api)
@@ -17,13 +23,34 @@ class Posts extends React.Component {
         })
       }
 
-      changeNewPost = (event) => {
-        this.setState({ newPost: event.target.value })
+      // changeNewPost = (event) => {
+      //   this.setState({ newPost: event.target.value })
+      // }
+
+      handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        console.log(`${name} = ${value}`)
+        this.setState({newPost})
+       
+        // below is a guide
+        // var someProperty = {...this.state.someProperty}
+        // someProperty.flag = true;
+        // this.setState({someProperty})
       }
 
       createNewPost = (event) => {
         event.preventDefault()
-        axios.post(api, { post: this.state.newPost })
+        console.log(this.state.newPost.title)
+        console.log(this.state.newPost.image)
+        console.log(this.state.newPost.text)
+
+        let title = this.state.newPost.title
+        let image = this.state.newPost.image
+        let text = this.state.newPost.text
+
+        axios.post(api, { post: {title: title, image: image, text: text} })
         .then((response) => {
           const posts = [...this.state.posts, response.data]
           this.setState({ posts, newPost: '' })
@@ -36,7 +63,10 @@ class Posts extends React.Component {
                 <div>
                 <form onSubmit={this.createNewPost}>
                 <label>New Post:</label>
-                <input value={ this.state.newPost } onChange={ this.changeNewPost } />
+                <input name="title" value={ this.state.newPost.title } onChange={ this.handleInputChange } />
+                <input name="image" value={ this.state.newPost.image } onChange={ this.handleInputChange } />
+                <input name="text" value={ this.state.newPost.text } onChange={ this.handleInputChange } />
+                <button>Submit</button>
                 </form>
                 </div>
                    { this.state.posts.map((post) => <p>{ post.title }, { post.image }, { post.text }</p>) }
