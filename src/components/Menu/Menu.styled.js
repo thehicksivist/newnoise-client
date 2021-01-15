88371586e704ac9120febac3aab4f854cd1a4209
James <42342586+thehicksivist@users.noreply.github.com>
2021-01-15 16:04:19 +1000

    height fix on menu drawer

--- src/components/Menu/Menu.styled.js
+++ src/components/Menu/Menu.styled.js
@@ -7,7 +7,7 @@ export const StyledMenu = styled.ul`
         flex-direction: column;
         justify-content: center;
         width: 100%;
-        height: 100%;
+        height: 100vh;
         background: white;
         opacity: 0.9;
         text-align: left;
